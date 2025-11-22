a5cdbe5aa0290d09eee78178d03858fe5c79248f
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-11-22 20:22:34 +0530

    navigate programmatically & pass/access dynamic params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit() {
+    // snapshot use when component is loaded
+    this.user = {
+      id: this.route.snapshot.params['id'],
+      name: this.route.snapshot.params['name'],
+    };
+
+    /*
+    we need to use this when we are in the same component and want to access the params value dynamically without reloading the whole component
+    */
+    this.route.params.subscribe((params: Params) => {
+      this.user = {
+        id: params['id'],
+        name: params['name'],
+      };
+    });
+    console.log(this.user);
   }
 
 }
