5df880d31c1a51edd1a7c2285142e0822f445a5e
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-11-22 11:13:40 +0530

    Revert "initial-routing-setup and navigate through link"
    This reverts commit bfcfe6c58563dd50b678b06c90c629dce1b37b7c.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,13 +11,6 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'servers', component: ServersComponent}
-]
 
 @NgModule({
   declarations: [
@@ -32,7 +25,6 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
