720c9457068339a9eea2012673e06c3b4dcda848
Ashish Barthwal <125344587+Ashish13112001@users.noreply.github.com>
2025-11-23 12:20:20 +0530

    childRouting and carry queryParams into next route

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(
     private serversService: ServersService,
@@ -24,7 +25,9 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
 
     //For same page fetch async data in the URL (queryParams, fragment)
-    this.route.queryParams.subscribe(data => console.log('-----', data));
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams['allowEdit'] === '1' ? true:false;
+    });
     this.route.fragment.subscribe(data => console.log('=====',data));
 
     this.server = this.serversService.getServer(1);
